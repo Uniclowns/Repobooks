20f64bc2609a0d3206777c9506a36829cf465c92
amely11 <teresiaamelia12@gmail.com>
2024-03-06 15:50:20 +0700

    Menambahkan gambar di file book
    Menambahkan gambar di file book

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Book</title>
 </head>
 <body>
-    
+<div class="container">
+        <h1>Contoh Halaman Web dengan Gambar</h1>
+        <img src="https://cdn.gramedia.com/uploads/items/9786230010460_Cov_Semua_Bisa_Menjadi_Programmer_Laravel_Basic.jpg" alt="Buku Laravel">
+        <p>Buku dengan judul “Semua Bisa Menjadi Programmer Laravel Basic” ini merupakan buku dasar dalam mempelajari framework PHP dengan Laravel yang saat ini sedang populer. <br>
+        Dengan buku ini, tandanya Anda sudah belajar sampai tingkat menengah. Akan dibahas pula kelebihan dari script server programming dengan Laravel, serta materi lengkap dari dasar hingga menengah.</p>
+    </div>
 </body>
 </html>
